3da19d5ed5a57b2d114bacafab05f7502065600d
sraj <srinivas4raj@gmail.com>
2015-05-31 17:35:25 -0600

    Adding Waypoints class

--- arduino/libraries/MegaBotics/Waypoints.h
+++ arduino/libraries/MegaBotics/Waypoints.h
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2015, Aakash Sahai and other contributors, a list of which is
  * maintained in the associated repository. All rights reserved.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -20,64 +20,57 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *    + Redistributions of source code must retain the above copyright
  *      notice, this list of conditions and the following disclaimer.
  *    + Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
  *    + Neither the name of the copyright holder nor the names of its contributors
- *	may be used to endorse or promote products derived from this software
- *	without specific prior written permission.
+ *		may be used to endorse or promote products derived from this software
+ *		without specific prior written permission.
  */
 
-#include <stddef.h>
-#include <inttypes.h>
-#include <math.h>
-#include <Arduino.h>
-#include <Servo.h>
+/*
+ * Waypoints.h
+ *
+ *  Created on: May 29, 2015
+ *      Author: Srinivas Raj
+ */
 
-#define	RAD2DEG(_r)	(_r * RAD_TO_DEG)
-#define	DEG2RAD(_d)	(_d * DEG_TO_RAD)
+#ifndef MEGABOTICS_WAYPOINTS_H_
+#define MEGABOTICS_WAYPOINTS_H_
 
-#ifndef DEBUG
-#define DEBUG 0
-#endif
+#include <MegaBotics.h>
 
-#if DEBUG
-#define DBG_PRINT(x)	Serial.print(x)
-#define DBG_PRINTLN(x)	Serial.println(x)
-#else
-#define DBG_PRINT(x)
-#define DBG_PRINTLN(x)
-#endif
+class GPS;
+class Logger;
+class JoyStick;
+class Dispaly;
 
-#include "PushButton.h"
-#include "SpdtSwitch.h"
-#include "Port.h"
-#include "UPort.h"
-#include "SPort.h"
-#include "IPort.h"
-#include "LED.h"
-#include "Coord.h"
-#include "Sonar.h"
-#include "SmartRover.h"
-#include "Rover.h"
-#include "WiFi.h"
-#include "PwmIn.h"
-#include "PwmOut.h"
-#include "PwmMux.h"
-#include "Logger.h"
-#include "AHRS.h"
-#include "EepromStore.h"
-#include "WheelEncoder.h"
-#include "ConfigParser.h"
-#include "Display.h"
-#include "SDCard.h"
-#include "PID.h"
-#include "InputPanel.h"
-#include "Utils.h"
-#include "GPS.h"
-#include "JoyStick.h"
-#include "Waypoints.h"
+#define DEFAULT_WAYPOINT_FILE	"wp.txt"
 
+class Waypoints {
+public:
+	Waypoints();
+	virtual ~Waypoints();
+
+	int configWaypoints();
+	bool nextWaypoint(Geo2D& waypoint);
+	void open();
+	void close();
+
+	static Waypoints * getInstance() { return &_instance; }
+	static Waypoints & getReference() { return _instance; }
+
+private:
+	static Waypoints _instance;
+
+	GPS* _gps;
+	Display* _display;
+	Logger* _logger;
+	JoyStick* _joyStick;
+	File _file;
+};
+
+#endif /* MEGABOTICS_WAYPOINTS_H_ */
